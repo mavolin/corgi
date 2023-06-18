580f5916dec51cd5e019211dff67168bae512561
mavolin <github@mavolin.co>
2023-06-18 20:26:52 +0200

    feat(cmd/corgidebug): add corgidebug

---
+++ cmd/corgidebug/corgidebug.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/k0kubun/pp"
+	"github.com/mattn/go-isatty"
+
+	"github.com/mavolin/corgi/corgierr"
+	"github.com/mavolin/corgi/parse"
+	"github.com/mavolin/corgi/validate"
+)
+
+func main() {
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now()
+	f, err := parse.Parse(data)
+
+	pp.Println(f)
+
+	fmt.Printf("\nparse took %s\n\n", time.Since(now))
+
+	if err != nil {
+		errs := err.(corgierr.List)
+		for _, err := range errs {
+			fmt.Println("")
+			fmt.Println(err.Pretty(corgierr.PrettyOptions{
+				Colored: isatty.IsTerminal(os.Stdout.Fd()),
+			}))
+		}
+
+		return
+	}
+
+	now = time.Now()
+	err = validate.UseNamespaces(f)
+	fmt.Printf("use namespace validation took %s\n\n", time.Since(now))
+	if err != nil {
+		errs := err.(corgierr.List)
+		for _, err := range errs {
+			fmt.Println("")
+			fmt.Println(err.Pretty(corgierr.PrettyOptions{
+				Colored: isatty.IsTerminal(os.Stdout.Fd()),
+			}))
+		}
+
+		return
+	}
+
+	now = time.Now()
+	err = validate.Validate(f)
+	fmt.Printf("validation took %s\n\n", time.Since(now))
+	if err != nil {
+		errs := err.(corgierr.List)
+		for _, err := range errs {
+			fmt.Println("")
+			fmt.Println(err.Pretty(corgierr.PrettyOptions{
+				Colored: isatty.IsTerminal(os.Stdout.Fd()),
+			}))
+		}
+
+		return
+	}
+}
