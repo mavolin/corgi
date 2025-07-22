7058317d3329520f800fb189881988fab25363a0
mavolin <github@mavolin.co>
2025-07-22 04:48:50 +0200

    refactor(load/link): less logging

--- load/link/load_imports.go
+++ load/link/load_imports.go
@@ -21,7 +21,7 @@ type importLoader struct {
 
 func (loader *importLoader) load(ctx context.Context, l *linker, logger *slog.Logger) {
 	logger = logger.WithGroup("import_loader")
-	logger.Info("Loading imports")
+	logger.Debug("Loading imports")
 
 	if l.importer == nil {
 		loader.localOnlyModeCheck(l, logger)
@@ -34,10 +34,8 @@ func (loader *importLoader) load(ctx context.Context, l *linker, logger *slog.Lo
 func (loader *importLoader) localOnlyModeCheck(l *linker, logger *slog.Logger) {
 	logger.Info("Running in local-only mode, not allowed to use imports")
 
-	logger.Debug("Checking for imports in package")
 	for _, f := range l.p.Files {
 		logger := logger.With(slog.String("file", f.Name))
-		logger.Debug("Checking file")
 
 		if len(f.Imports) == 0 {
 			continue
@@ -67,7 +65,6 @@ func (loader *importLoader) loadImports(ctx context.Context, l *linker, logger *
 
 	for _, f := range l.p.Files {
 		logger := logger.With(slog.String("file", f.Name))
-		logger.Debug("Loading imports for file")
 
 		for _, imp := range f.Imports {
 			if !imp.Explicit() || imp.Path == "" || imp.LoadedWithErrors {
@@ -77,7 +74,6 @@ func (loader *importLoader) loadImports(ctx context.Context, l *linker, logger *
 			logger := logger.With(
 				slog.String("import", imp.Path),
 				slog.String("import_pos", imp.AST.Start().String()))
-			logger.Debug("Loading import")
 
 			wg.Go(func() {
 				loader.loadImport(ctx, l, logger, f, imp)
@@ -85,16 +81,16 @@ func (loader *importLoader) loadImports(ctx context.Context, l *linker, logger *
 		}
 	}
 
-	logger.Info("Waiting for all goroutines to finish")
+	logger.Debug("Waiting for all goroutines to finish")
 	wg.Wait()
-	logger.Info("Finished loading imports")
+	logger.Debug("Finished loading imports")
 }
 
 func (loader *importLoader) loadImport(ctx context.Context, l *linker, logger *slog.Logger, f *file.File, imp *file.Import) {
 	logger = logger.With(
 		slog.String("import", imp.Path),
 		slog.String("import_pos", imp.AST.Start().String()))
-	logger.Info("Loading import")
+	logger.Debug("Loading import")
 
 	var d diagnostic.List
 	var err error
