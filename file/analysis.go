3062934accb0fddd9f9c3b43c467a0a455564dbb
mavolin <github@mavolin.co>
2025-08-01 14:58:42 +0200

    feat(file): add ConditionalAnalysis

--- file/analysis.go
+++ file/analysis.go
@@ -21,13 +21,25 @@ func FailedAnalysis[T comparable]() (a Analysis[T]) {
 	return a
 }
 
-// NotZero indicates whether the analysis was successful and the result is not
-// the zero value.
+// ConditionalAnalysis returns an analysis result that is dependent on the
+// condition analysis:
 //
-// Do not negate, the negation is probably not what you expect!
-func (a Analysis[T]) NotZero() bool {
-	var zero T
-	return !a.Failed && a.Result != zero
+// If the condition is not zero, result is returned.
+// If the condition or the result is zero, zero is returned.
+// In any other case, namely if the condition is not zero and the result failed,
+// or if the condition failed and the result is not zero or failed, a failed
+// analysis is returned.
+func ConditionalAnalysis[C, R comparable](condition Analysis[C], result Analysis[R]) (final Analysis[R]) {
+	var cZero C
+	var rZero R
+	switch {
+	case condition.NotZero():
+		return result
+	case condition.Equal(cZero) || result.Equal(rZero):
+		return Result(rZero)
+	default:
+		return FailedAnalysis[R]()
+	}
 }
 
 func (a Analysis[T]) GetOr(fallback T) T {
@@ -59,6 +71,15 @@ func (a *Analysis[T]) SetFailed() {
 func (a *Analysis[T]) SetZero() {
 	var zero T
 	a.Result, a.Failed = zero, false
+}
+
+// NotZero indicates whether the analysis was successful and the result is not
+// the zero value.
+//
+// Do not negate, the negation is probably not what you expect!
+func (a Analysis[T]) NotZero() bool {
+	var zero T
+	return !a.Failed && a.Result != zero
 }
 
 func (a Analysis[T]) Equal(v T) bool {
