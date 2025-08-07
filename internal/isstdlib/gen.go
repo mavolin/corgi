07f2cdb17362429ea33490ebc0ec873d0ad0b4a4
mavolin <github@mavolin.co>
2025-08-07 19:42:58 +0200

    refactor(tools/stdlibdetector): move codegen into a isstdlib test

--- internal/isstdlib/gen.go
+++ internal/isstdlib/gen.go
@@ -1,3 +1,6 @@
 package isstdlib
 
-//go:generate go run github.com/mavolin/corgi/v2/tools/codegen/stdlibdetector detector.go
+// This file only exists to generate the detector.go file through the Generate
+// "test".
+
+//go:generate go test -run=Generate -tags=generate
