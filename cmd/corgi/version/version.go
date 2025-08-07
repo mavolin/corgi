0f51e814e5ad6c951dff8f460f786ea7e0f1f355
mavolin <github@mavolin.co>
2025-08-07 17:57:23 +0200

    feat(cmd/version): add version command

---
+++ cmd/corgi/version/version.go
@@ -0,0 +1,32 @@
+// Package version provides the version subcommand.
+package version
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mavolin/corgi/v2/cmd/corgi/command"
+	buildmeta "github.com/mavolin/corgi/v2/internal/meta"
+)
+
+var (
+	meta = command.Meta{
+		Name:             "version",
+		ShortDescription: "Display the version of the CLI.",
+		LongDescription:  "Display the version of the CLI.",
+		ArgUsages: []string{
+			"display the version of the CLI",
+		},
+	}
+
+	Command = command.Command[*command.NoFlags](meta, nil, run)
+)
+
+func run(_ *command.Cmd, _ *command.NoFlags, args []string) {
+	if len(args) != 0 {
+		fmt.Fprintln(os.Stderr, "expected no arguments")
+		os.Exit(2)
+	}
+
+	fmt.Printf("corgi version %s\n", buildmeta.Version)
+}
