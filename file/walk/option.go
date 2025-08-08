87b9d7a2785f6ed3372c09ab9aaa2fb565705f1d
mavolin <github@mavolin.co>
2025-08-08 13:57:23 +0200

    refactor(file/walk): remove `TopLevel`

--- file/walk/option.go
+++ file/walk/option.go
@@ -1,9 +1,6 @@
 package walk
 
 import (
-	"fmt"
-
-	"github.com/mavolin/corgi/v2/file"
 	"github.com/mavolin/corgi/v2/file/ast"
 )
 
@@ -20,39 +17,6 @@ import (
 // Note that an Option is called on every item, even if the walk function
 // is typed.
 type Option func(*Context) error
-
-// TopLevel prevents the function from diving into:
-//   - Elements
-//   - Component call block setters where no instance of the
-//     referenced block top-level.
-//
-// Requires component calls to be analyzed and therefore, transitively, the
-// file's symbols to be built.
-//
-// Ignores withs not linked to a block.
-func TopLevel(f *file.File) Option {
-	return func(ctx *Context) error {
-		switch n := ctx.Node.(type) {
-		case *ast.Element:
-			return Skip
-		case *ast.With:
-			astCC := Closest[*ast.ComponentCall](ctx.Parents)
-			cc := f.ComponentCallByNode(astCC)
-			if cc == nil {
-				panic(fmt.Sprintf("walk.TopLevel called without building symbols: %s:%s: file.ComponentCall not found for ast node", f.ModulePath(), cc.AST.Start()))
-			}
-			blockSetter := cc.BlockSetterByNode(n)
-			if blockSetter == nil {
-				panic(fmt.Sprintf("walk.TopLevel called without analyzing component calls: %s:%s: file.BlockSetter not found for ast node", f.ModulePath(), n.Start()))
-			}
-			if blockSetter.Block == nil || blockSetter.Block.TopLevel(file.AtLeastOne).Equal(false) {
-				return Skip
-			}
-			return nil
-		}
-		return nil
-	}
-}
 
 func DontDive[N ast.Node]() Option {
 	return func(ctx *Context) error {
