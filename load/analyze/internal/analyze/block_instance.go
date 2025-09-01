ecaebb9d6a278ae7bc05c0fcfe3930e248036687
mavolin <github@mavolin.co>
2025-09-02 01:56:36 +0200

    feat(load/analyze): BlockInstance: ContainingElements

--- load/analyze/internal/analyze/block_instance.go
+++ load/analyze/internal/analyze/block_instance.go
@@ -3,6 +3,7 @@ package analyze
 import (
 	"context"
 	"log/slog"
+	"slices"
 
 	"github.com/mavolin/corgi/v2/file"
 	"github.com/mavolin/corgi/v2/file/ast"
@@ -24,7 +25,7 @@ func (z *analyzer) AnalyzeBlockInstance(
 		With(slog.String("block", bi.Group.Name))
 
 	z.AnalyzeBlockInstanceForwarded(ctx, c, parents, bi)
-
+	z.AnalyzeBlockInstanceContainingElements(ctx, c, parents, bi)
 }
 
 // ============================================================================
@@ -79,6 +80,78 @@ func (z *analyzer) AnalyzeBlockInstanceForwarded(ctx context.Context, c *file.Co
 		}
 		i--
 	}
+}
+
+// ============================================================================
+// Containing Elements
+// ======================================================================================
+
+// AnalyzeBlockInstanceContainingElements determines all elements containing
+// the given block instance.
+//
+// Depends on Checks: None
+//
+// Sets Fields:
+//   - Components.Blocks.Instances.ContainingElements
+//
+// Depends on Fields: None
+func (z *analyzer) AnalyzeBlockInstanceContainingElements(ctx context.Context, c *file.Component, parents []*walk.Context, bi *file.BlockInstance) {
+	var containingElements []file.ContainingElement
+
+	i := len(parents) - 1
+	for i >= 0 {
+		parent := parents[i]
+		switch parent := parent.Node.(type) {
+		case *ast.ComponentCall: // we're filling the cc's &-placeholder
+			bi.ContainingElements.SetFailed()
+			return
+		case ast.BlockSetter:
+			ccI := walk.ClosestIndex[*ast.ComponentCall](parents[:i])
+			if ccI < 0 {
+				bi.ContainingElements.SetFailed()
+				return
+			}
+
+			ccAST := parents[ccI].Node.(*ast.ComponentCall) //nolint:errcheck
+			cc := c.File.ComponentCallByNode(ccAST)
+			z.AnalyzeComponentCall(ctx, cc)
+
+			s := cc.BlockSetterByName(parent.Name())
+			if s == nil || s.Block == nil || s.Block.ContainingElements.Failed() {
+				bi.ContainingElements.SetFailed()
+				return
+			}
+
+			containingElements = append(containingElements, *s.Block.ContainingElements.Result()...)
+
+			if s.Block.Forwarded.False() {
+				containingElements = slices.Clip(containingElements)
+				bi.ContainingElements.SetResult(&containingElements)
+				return
+			}
+			i = ccI - 1 // continue with the parent of the component call
+			continue
+		case *ast.Element:
+			compAST := walk.Closest[*ast.Component](parents)
+			if compAST == nil {
+				bi.ContainingElements.SetFailed()
+				return
+			}
+			comp := c.File.Package.ComponentByNode(compAST)
+
+			containingElements = append(containingElements, file.ContainingElement{
+				Component: comp,
+				Element:   comp.File.ElementReferenceByNode(parent.Header.Name),
+			})
+			containingElements = slices.Clip(containingElements)
+			bi.ContainingElements.SetResult(&containingElements)
+			return
+		}
+		i--
+	}
+
+	containingElements = slices.Clip(containingElements)
+	bi.ContainingElements.SetResult(&containingElements)
 }
 
 // ============================================================================
