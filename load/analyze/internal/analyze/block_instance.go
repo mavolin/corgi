e38a470ded7521f65f82b6e09ad96b2209d3905c
mavolin <github@mavolin.co>
2025-09-02 01:30:35 +0200

    feat(load/analyze): BlockInstance: Forwarded

--- load/analyze/internal/analyze/block_instance.go
+++ load/analyze/internal/analyze/block_instance.go
@@ -17,16 +17,13 @@ import (
 //
 // Depends on Fields: None
 func (z *analyzer) AnalyzeBlockInstance(
-	ctx context.Context, logger *slog.Logger, c *file.Component, parents []*walk.Context, b *file.BlockInstance,
+	ctx context.Context, logger *slog.Logger, c *file.Component, parents []*walk.Context, bi *file.BlockInstance,
 ) {
-	block := c.BlockByNode(biAST)
-
 	logger = logger.
 		WithGroup("blocks").
-		With(slog.String("block", block.Name))
+		With(slog.String("block", bi.Group.Name))
 
-	z.AnalyzeBlockInstanceParentInformation(ctx, c, parents)
-	z.AnalyzeBlockInstanceCannotForwardAttributes(c, cannotAttributes, n)
+	z.AnalyzeBlockInstanceForwarded(ctx, c, parents, bi)
 
 }
 
@@ -34,16 +31,8 @@ func (z *analyzer) AnalyzeBlockInstance(
 // Forwarded
 // ======================================================================================
 
-func (z *analyzer) AnalyzeBlockInstancesForwarded(b *file.Block) {
-
-}
-
-func (z *analyzer) analyzeBlockInstanceForwarded(b *file.Block) {
-
-}
-
-// AnalyzeBlockInstanceParentInformation determines whether the parent
-// information of the given block instance.
+// AnalyzeBlockInstanceForwarded determines whether the given block instance
+// is forwarded (i.e. placed outside any element).
 //
 // Depends on Checks: None
 //
@@ -51,37 +40,21 @@ func (z *analyzer) analyzeBlockInstanceForwarded(b *file.Block) {
 //   - Components.Blocks.Instances.Forwarded
 //
 // Depends on Fields: None
-func (z *analyzer) AnalyzeBlockInstanceParentInformation(ctx context.Context, c *file.Component, parents []*walk.Context, biAST *ast.Block) {
-	bi := c.BlockInstanceByNode(biAST)
-	if bi == nil {
-		return
-	}
-
-	var (
-		element *file.ElementReference
-		cc      *file.ComponentCall
-		block   *file.Block
-	)
+func (z *analyzer) AnalyzeBlockInstanceForwarded(ctx context.Context, c *file.Component, parents []*walk.Context, bi *file.BlockInstance) {
 	bi.Forwarded.SetResult(true)
 
 	i := len(parents) - 1
-Loop:
 	for i >= 0 {
 		parent := parents[i]
 		switch parent := parent.Node.(type) {
 		case *ast.ComponentCall:
+			// Continue checking: if the instance has another element as
+			// parent, we can still be sure it's not forwarded.
 			bi.Forwarded.SetFailed()
-			bi.ContainingElements.SetFailed()
-			return
 		case ast.BlockSetter:
-			if cc != nil {
-				continue
-			}
-
 			ccI := walk.ClosestIndex[*ast.ComponentCall](parents[:i])
 			if ccI < 0 {
 				bi.Forwarded.SetFailed()
-				bi.ContainingElements.SetFailed()
 				continue
 			}
 
@@ -91,53 +64,21 @@ Loop:
 
 			s := cc.BlockSetterByName(parent.Name())
 			if s == nil || s.Block == nil {
+				// Continue checking: if the instance has another element as
+				// parent, we can still be sure it's not forwarded.
 				bi.Forwarded.SetFailed()
-				bi.ContainingElements.SetFailed()
 			} else if s.Block.Forwarded.False() {
 				bi.Forwarded.SetResult(false)
-				break Loop
+				return
 			}
 			i = ccI - 1 // continue with the parent of the component call
 			continue
 		case *ast.Element:
 			bi.Forwarded.SetResult(false)
-			element = c.File.ElementReferenceByNode(parent.Header.Name)
-			break Loop
-		}
-		i--
-
-	}
-
-	if bi.ContainingElements.Failed() {
-		return
-	} else if cc == nil {
-		var containingElements []*file.ElementReference
-		if element != nil {
-			containingElements = []*file.ElementReference{element}
-		}
-		bi.ContainingElements.SetResult(&containingElements)
-		return
-	}
-
-	containingElements := make([]*file.ElementReference, 0, 1+len(block.Instances))
-	if block.Forwarded.True() && element != nil {
-		containingElements = append(containingElements, element)
-	}
-
-	added := make(map[*file.ElementReference]bool)
-	for _, inst := range block.Instances {
-		if inst.ContainingElements.Failed() {
-			bi.ContainingElements.SetFailed()
 			return
 		}
-		for _, el := range *inst.ContainingElements.Result() {
-			if !added[el] {
-				containingElements = append(containingElements, el)
-				added[el] = true
-			}
-		}
+		i--
 	}
-	bi.ContainingElements.SetResult(&containingElements)
 }
 
 // ============================================================================
