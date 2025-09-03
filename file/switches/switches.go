3a195dabe1f0efb81114f7754fe7c004e148c21f
mavolin <github@mavolin.co>
2025-09-03 12:49:47 +0200

    docs(file/switches): clarify role of package in versioning

--- file/switches/switches.go
+++ file/switches/switches.go
@@ -2,6 +2,15 @@
 // types in package ast.
 //
 // The switching functions are generated using go generate.
+//
+// Since the sum types may evolve over time, getting extended with new types,
+// this package is expressly exempt from the usual stability guarantee.
+// More precisely, a minor version increment allows a breaking change in the
+// signatures of the switching functions.
+// This, of course, is intended breakage to warn users relying on an exhaustive
+// switch that they need to handle a new type.
+// All other compatibility guarantees remain untouched.
+// Patch version increments do not allow breaking changes.
 package switches
 
 //go:generate go test -run=Generate -tags=generate
