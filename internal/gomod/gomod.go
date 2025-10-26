9e26f3fdf0af1833d5f744a2fd028d6d28c71152
mavolin <github@mavolin.co>
2025-10-26 16:23:06 +0100

    docs(internal/gomod): improve docs

--- internal/gomod/gomod.go
+++ internal/gomod/gomod.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/mod/modfile"
 )
 
-// Find attempts to find the go.mod file governing this directory by first
+// Find attempts to find the go.mod file governing the given directory by first
 // searching for it in dir and then in dir's parents.
 //
 // dir must be an absolute path in using the system's path separator.
