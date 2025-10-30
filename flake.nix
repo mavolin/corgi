27e9a7f521202e4e4724be8858508d68b09a17a0
mavolin <github@mavolin.co>
2025-10-30 14:31:14 +0100

    ci: use flake/direnv in CI

--- flake.nix
+++ flake.nix
@@ -22,13 +22,34 @@
       {
         formatter = pkgs.nixfmt-rfc-style;
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            go
-            gotools
-            util-linux
-          ];
+          packages =
+            with pkgs;
+            with self.packages.${system};
+            [
+              go
+              gocovmerge
+              golangci-lint
+              goreleaser
+              gotools
+              util-linux
+            ];
 
           hardeningDisable = [ "fortify" ];
+        };
+
+        packages = {
+          gocovmerge = pkgs.buildGoModule rec {
+            pname = "gocovmerge";
+            version = "2023.05.07+${commit}";
+            commit = "fa4f82c";
+            src = pkgs.fetchFromSourcehut {
+              owner = "~shabbyrobe";
+              repo = pname;
+              rev = commit;
+              hash = "sha256-Q2RcwVAzNe8rgwVRY5gAOZEEBkeUoaYctUvCUkM0eWk=";
+            };
+            vendorHash = "sha256-E+LaOf3Ewl5oBvJjSoLTHDj1yFgDOVR5v520Ca5UbNc=";
+          };
         };
       }
     );
