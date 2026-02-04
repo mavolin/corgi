362010249cc7fabba88e29de7b9b138f2764623d
mavolin <github@mavolin.co>
2026-02-04 15:15:40 +0100

    docs(internal/meta): CLI: add doc explaining purpose

--- internal/meta/meta.go
+++ internal/meta/meta.go
@@ -6,6 +6,25 @@ import (
 	"strings"
 )
 
+// CLI indicates that the binary being run is the CLI defined in package cmd.
+//
+// We use this flag to give more concrete internal error messages:
+// Internal errors indicate that something is wrong with this module.
+// Most internal errors should never occur regardless of context.
+// However, for some errors it is ambiguous whether they occurred because of
+// a bug in this module, or due to incorrect usage by a consumer of the library.
+//
+// For the CLI, it is clear that regardless of whether the error arose from a
+// bug or by incorrect usage, code in this module is at fault.
+// As such, we want an issue to be filed with this module's issue tracker,
+// regardless.
+//
+// However, when consumed as a library, we want to indicate to the consumer
+// that the error might have been caused by them, and not necessarily due to a
+// bug.
+// In such a case, instead of always asking for an issue to be filed, we can
+// explain how else the error might have occurred, so the consumer can
+// ascertain whether the bug lies with them or with this module.
 var CLI = false
 
 // develVersion is the version string used for development builds.
